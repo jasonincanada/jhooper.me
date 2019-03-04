ff3d525287c7a83f00b9ca95adc8513a87bbfea7
jasonincanada <nirgle@gmail.com>
2019-03-04 12:21:55 -0500

    Rearrange code and add comments

--- src/ReportCards.hs
+++ src/ReportCards.hs
@@ -1,3 +1,16 @@
+{- ReportCards.hs
+
+   This module exports interpolateReportBody, which looks through the body of a
+   file in school-years/*.md for a [report] segment, eg:
+
+     [report:Grade-5-Term-1.jpg:Progress Report - November]
+
+   and converts it to an HTML <li> element, eg:
+
+     <li><a href="images/report-cards/Grade-5-Term-1.jpg">Progress Report - November</a></li>
+
+-}
+
 module ReportCards (
   interpolateReportBody
 ) where
@@ -10,27 +23,29 @@ data ReportBody = Character Char
                 | Report String String
                 deriving (Show)
 
-template = "<li>"
-           ++ "<a href=\"images/report-cards/%s\">%s</a>" ++
-           "</li>"
-
-reportBody :: Parser [ReportBody]
-reportBody = many (reportReport <|> reportChar)
-
-reportReport :: Parser ReportBody
-reportReport = Report <$> (string "[report:" *> notChar ':')
-                      <*> (char ':' *> notChar ']' <* char ']')
-  where
-    notChar :: Char -> Parser String
-    notChar c = many $ satisfy (/=c)
-
-reportChar :: Parser ReportBody
-reportChar = Character <$> item
-
+-- concatMap :: Foldable t => (a -> [b]) -> t a -> [b]
 interpolateReportBody :: String -> String
-interpolateReportBody src = concatMap toString (run reportBody src)
+interpolateReportBody body = concatMap toString parsed
   where
     toString :: ReportBody -> String
     toString (Character c)          = [c]
     toString (Report filename desc) = printf template filename desc
+
+    template = "<li>"
+               ++ "<a href=\"images/report-cards/%s\">%s</a>" ++
+               "</li>"
+
+    parsed :: [ReportBody]
+    parsed = run reportBody body
+
+    reportBody :: Parser [ReportBody]
+    reportBody = many (reportReport <|> reportChar)
+      where
+        reportChar, reportReport :: Parser ReportBody
+        reportChar   = Character <$> item
+        reportReport = Report <$> (string "[report:" *> notChar ':')
+                              <*> (string ":"        *> notChar ']' <* char ']')
+
+        notChar :: Char -> Parser String
+        notChar c = many $ satisfy (/=c)
 
